795ed73fe0cbc110287c2d48a01afdb6b4a49512
Naomi Aro <naomiaro@gmail.com>
2016-03-12 16:28:13 +0100

    Using babel to compile

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.linear = linear;
-exports.exponential = exponential;
-exports.sCurve = sCurve;
-exports.logarithmic = logarithmic;
-function linear(length, rotation) {
+export function linear(length, rotation) {
     var curve = new Float32Array(length),
-        i,
-        x,
+        i, x,
         scale = length - 1;
 
     for (i = 0; i < length; i++) {
@@ -18,25 +10,25 @@ function linear(length, rotation) {
 
         if (rotation > 0) {
             curve[i] = x;
-        } else {
-            curve[i] = 1 - x;
         }
+        else {
+            curve[i] = 1 - x;
+        }  
     }
 
     return curve;
 }
 
-function exponential(length, rotation) {
+export function exponential(length, rotation) {
     var curve = new Float32Array(length),
-        i,
-        x,
+        i, x,
         scale = length - 1,
         index;
 
     for (i = 0; i < length; i++) {
         x = i / scale;
         index = rotation > 0 ? i : length - 1 - i;
-
+       
         curve[index] = Math.exp(2 * x - 1) / Math.exp(1);
     }
 
@@ -44,19 +36,19 @@ function exponential(length, rotation) {
 }
 
 //creating a curve to simulate an S-curve with setValueCurveAtTime.
-function sCurve(length, rotation) {
+export function sCurve(length, rotation) {
     var curve = new Float32Array(length),
         i,
-        phase = rotation > 0 ? Math.PI / 2 : -(Math.PI / 2);
+        phase = (rotation > 0) ? Math.PI/2 : -(Math.PI/2);
 
     for (i = 0; i < length; ++i) {
-        curve[i] = Math.sin(Math.PI * i / length - phase) / 2 + 0.5;
+        curve[i] = (Math.sin((Math.PI * i / length) - phase)) / 2 + 0.5;
     }
     return curve;
 }
 
 //creating a curve to simulate a logarithmic curve with setValueCurveAtTime.
-function logarithmic(length, base, rotation) {
+export function logarithmic(length, base, rotation) {
     var curve = new Float32Array(length),
         index,
         x = 0,
